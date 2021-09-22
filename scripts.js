c2eb704e837a8c2cc054f1382b872d92e866776f
martinsauseklis <martinamauseklim@gmail.com>
2021-09-22 13:49:08 +0300

    second section

---
+++ scripts.js
@@ -0,0 +1,15 @@
+/*const tennisImg = document.getElementById('tennis')
+
+tennisImg.addEventListener("mouseover", () => {
+    document.getElementById("tennis-container").style.gridArea = "1/1/2/11"
+    document.getElementById("tennis-descr").style.gridArea = "1/11/2/13"
+    document.getElementById("tennis-p").style.display = "none"
+})
+
+tennisImg.addEventListener("mouseout", () => {
+    document.getElementById("tennis-container").style.gridArea = "1/1/2/8"
+    document.getElementById("tennis-descr").style.gridArea = "1/8/2/13"
+    document.getElementById("tennis-p").style.display = "block"
+})
+
+*/
