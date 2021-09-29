c16480c206c9ef175185fcefd5993b7fcbdd1da8
martinsauseklis <martinamauseklim@gmail.com>
2021-09-29 09:33:52 +0300

    script fix

--- scripts.js
+++ scripts.js
@@ -1,25 +1,34 @@
 function onMouseOver(id, id2) {
-    document.getElementById(id).style.gridColumnStart = "1"
-    document.getElementById(id).style.gridColumnEnd = "13"
-    document.getElementById(id2).style.display = "none"
+    if (document.getElementById(id).style.gridColumnEnd === "8") {
+        document.getElementById(id).style.gridColumnStart = "1"
+        document.getElementById(id).style.gridColumnEnd = "13"
+        document.getElementById(id2).style.display = "none"
+    }
+    
 }
 
 function onMouseOut(id, id2) {
-    document.getElementById(id).style.gridColumnStart = "1"
-    document.getElementById(id).style.gridColumnEnd = "8"
-    document.getElementById(id2).style.display = "flex"
+    if (document.getElementById(id).style.gridColumnEnd === "13") {
+        document.getElementById(id).style.gridColumnStart = "1"
+        document.getElementById(id).style.gridColumnEnd = "8"
+        document.getElementById(id2).style.display = "flex"
+    }
 }
 
 function onMouseOverOpp(id, id2) {
-    document.getElementById(id).style.gridColumnStart = "1"
-    document.getElementById(id).style.gridColumnEnd = "13"
-    document.getElementById(id2).style.display = "none"
+    if (document.getElementById(id).style.gridColumnStart === "6") {
+        document.getElementById(id).style.gridColumnStart = "1"
+        document.getElementById(id).style.gridColumnEnd = "13"
+        document.getElementById(id2).style.display = "none"
+    }
 }
 
 function onMouseOutOpp(id, id2) {
-    document.getElementById(id).style.gridColumnStart = "6"
-    document.getElementById(id).style.gridColumnEnd = "13"
-    document.getElementById(id2).style.display = "flex"
+    if (document.getElementById(id).style.gridColumnStart === "1") {
+        document.getElementById(id).style.gridColumnStart = "6"
+        document.getElementById(id).style.gridColumnEnd = "13"
+        document.getElementById(id2).style.display = "flex"
+    }
 }
 
 function showMenu() {
